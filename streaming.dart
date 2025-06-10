aa339462ccac8618a6162c86e7775b486f9bae90
Linda Sixtus <linda.sixtus@gmx.de>
2025-06-10 14:00:40 +0200

    Aufgabe 2 - streamingdienst weil zu jung

---
+++ streaming.dart
@@ -0,0 +1,22 @@
+void main(){
+  bool isLoggedIn = true;
+  bool isBanned = true;
+  bool isSubscribed = true;
+  int age = 18;
+
+  if (!isLoggedIn){
+    print ("Bitte logge dich ein");
+  }
+  else if (isBanned){
+    print ("Dein Account wurde gesperrt");
+  }
+  else if (!isSubscribed){
+    print ("Bitte abonniere");
+  }
+  else if (age<18){
+    print ("Du bist zu jung");
+  }
+  else {
+    print ("Willkommen");
+  }
+}
